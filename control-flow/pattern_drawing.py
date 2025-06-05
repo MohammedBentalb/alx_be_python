282eb0ee7055f7d2e4208ce40842358a018f4692
mohammed bentalb <mohammedbentalb2000@gmail.com>
2025-06-05 12:56:59 +0100

    fixing loop

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,4 +1,9 @@
 size = int(input('Enter the size of the pattern: '))
 
-for i in range(1, size + 1):
-    print('*' * size)
+loops = 0
+
+while loops < size:
+    for i in range(1, size + 1):
+        print('*', end='')
+    print('\n')
+    loops += 1
